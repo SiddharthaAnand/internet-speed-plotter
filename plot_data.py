b6680cc66172db6405a12757479558b9bc57bea8
Joel Rodriguez <joelerll@gmail.com>
2018-10-21 03:16:14 -0500

    Plot the data of the speed being collected

--- plot_data.py
+++ plot_data.py
@@ -1,12 +1,36 @@
-import matplotlib.pyplot as plot
-import matplotlib.animation as animation
+from datetime import datetime
+import csv
+import numpy as np
+import itertools
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import time
 
-figure = plot.figure()
-axis = figure.add_subplot(1, 1, 1)
+speeds = []
+dates = []
 
-def animate_plot(x):
-	axis.clear()
-	axis.plot(x)
+with open('speed.csv', 'rb') as csvfile:
+	lines = itertools.islice(csvfile, 1, None)
+	spamreader = csv.reader(lines, delimiter=',', quotechar='|')
+	for row in spamreader:
+		speed = row[0]
+		date_str = row[2]
+		dt_obj = datetime.strptime(date_str, "%a %b %d %X %Y")
+		speeds.append(speed)
+		dates.append(dt_obj)
 
-ani = animation.FuncAnimation(figure, animate_plot, interval = 60)
-plot.show()
+fig, ax = plt.subplots()
+ax.plot(dates, speeds)
+datemin = np.datetime64(dates[0], 'm')
+datemax = np.datetime64(dates[-1], 'm') + np.timedelta64(1, 'm')
+ax.set_xlim(datemin, datemax)
+
+def mbs(x):
+    return x
+ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+ax.format_ydata = mbs
+ax.grid(True)
+
+fig.autofmt_xdate()
+
+plt.show()
